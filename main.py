3b5707ded4398ee33517522aa76975b65afea67d
bndsknzlr <a.sondermann@mac.com>
2022-04-02 16:37:02 +0200

    mit sound

--- main.py
+++ main.py
@@ -3,19 +3,13 @@ from snake import Snake
 from food import Food
 from scoreboard import Scoreboard
 import time
-
-from vlc import MediaPlayer, PlaybackMode
-
-p = MediaPlayer("/Users/achimsondermann/Music/Splice/sounds/packs/Chiptune/SM_White_Label_-_Chiptune_-_Wav/drum_loops/ct_drm128_ascension_top.mp3")
-pm = PlaybackMode(1)
-
-p.play()
+import vlc
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.colormode(255)
 screen.bgcolor("black")
-screen.title("My Snake Game")
+screen.title("Tolstoi The Snake")
 screen.tracer(0)
 
 snake = Snake()
@@ -29,9 +23,13 @@ screen.onkey(fun=snake.down, key="Down")
 screen.onkey(fun=snake.b_left, key="Left")
 screen.onkey(fun=snake.b_right, key="Right")
 
+playlist = ["/Users/achimsondermann/Music/Splice/sounds/packs/"
+            "Chiptune/""SM_White_Label_-_Chiptune_-_Wav/drum_loops/ct_drm128_ascension_top.mp3"]
+
+player = vlc.MediaPlayer(playlist[0])
 game_on = True
-collisions = 0
 while game_on:
+    player.play()
     screen.update()
     time.sleep(0.1)
     snake.move()
@@ -41,10 +39,19 @@ while game_on:
         snake.extend()
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         game_on = False
+        player.stop()
         scoreboard.game_over()
+        player = vlc.MediaPlayer("/Users/achimsondermann/Music/Splice/sounds/packs/Chiptune/"
+                                 "SM_White_Label_-_Chiptune_-_Wav/fx/fallers/ct_fx_eraser.wav")
+        player.play()
     for collide in snake.segments[1:]:
         if snake.head.distance(collide) < 15:
             game_on = False
+            player.stop()
             scoreboard.game_over()
+            scoreboard.game_over()
+            player = vlc.MediaPlayer("/Users/achimsondermann/Music/Splice/sounds/packs/Chiptune/"
+                                     "SM_White_Label_-_Chiptune_-_Wav/fx/fallers/ct_fx_eraser.wav")
+            player.play()
 
 screen.exitonclick()
